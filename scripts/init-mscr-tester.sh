1a81130c4304a954d8146f6e1d486c03b98f655e
Joonas Kesäniemi <joonas.kesaniemi@csc.fi>
2024-10-31 14:52:22 +0200

    Add separate group admin and superuser without groups

--- scripts/init-mscr-tester.sh
+++ scripts/init-mscr-tester.sh
@@ -2,10 +2,12 @@
 #
 #
 #----------------------------------
-podman exec yti-postgres psql -U postgres -d groupmanagement -c "INSERT INTO public.user VALUES ('admin@localhost','Admin','User',true,null,null,'4ce70937-6fa4-49af-a229-b5f10328adb8')"
+podman exec yti-postgres psql -U postgres -d groupmanagement -c "INSERT INTO public.user VALUES ('admin@localhost','Admin','User',false,null,null,'4ce70937-6fa4-49af-a229-b5f10328adb8')"
 podman exec yti-postgres psql -U postgres -d groupmanagement -c "INSERT INTO public.user VALUES ('editor@localhost','Editor','User',false,null,null,'4ce70937-6fa4-49af-a229-b5f10328adb7')"
 podman exec yti-postgres psql -U postgres -d groupmanagement -c "INSERT INTO public.user VALUES ('member@localhost','Member','User',false,null,null,'4ce70937-6fa4-49af-a229-b5f10328adb6')"
 podman exec yti-postgres psql -U postgres -d groupmanagement -c "INSERT INTO public.user VALUES ('outsider@localhost','Outsider','User',false,null,null,'4ce70937-6fa4-49af-a229-b5f10328adb5')"
+podman exec yti-postgres psql -U postgres -d groupmanagement -c "INSERT INTO public.user VALUES ('super@localhost','Super','User',true,null,null,'4ce70937-6fa4-49af-a229-b5f10328adb4')"
+
 #----------------------------------
 #----------------------------------
 podman exec yti-postgres psql -U postgres -d groupmanagement -c "INSERT INTO public.organization (id,url,name_en,name_fi,name_sv) VALUES ('7d3a3c00-5a6b-489b-a3ed-63bb58c26a63','https://yhteentoimiva.suomi.fi/','Interoperability platform developers','Yhteentoimivuusalustan yllapito','Utvecklare av interoperabilitetsplattform')"
